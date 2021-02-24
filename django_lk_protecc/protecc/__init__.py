8919c54d6b0f1375380072a5ef44a21e17fda6b6
drewrasm <drewrasm@gmail.com>
2021-02-24 08:52:58 -0700

    move the init to the right place

--- django_lk_protecc/protecc/__init__.py
+++
@@ -1,8 +0,0 @@
-__title__ = 'django_lk_protecc'
-__version__ = '1.0.0'
-__author__ = 'lightningkite'
-__license__ = 'tba'
-__copyright__ = 'Copyright 2021 lightningkite'
-
-# version synonym 
-VERSION = __version__
