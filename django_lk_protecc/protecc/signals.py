02070adf3ca3e9c29a49af847be0b4812b30da53
Andrew Rasmussen <drewrasm@gmail.com>
2021-07-15 13:48:24 -0600

    add utils file

--- django_lk_protecc/protecc/signals.py
+++ django_lk_protecc/protecc/signals.py
@@ -4,8 +4,10 @@ from .models import WhiteListTracker, FraudTracker
 from django.conf import settings
 from django.core.cache import cache
 from django.core.mail import send_mail
-import requests
-import json
+from datetime import datetime, timedelta
+from .utils import block_ip_address
+# import requests
+# import json
 
 @receiver(post_save, sender=FraudTracker)
 def handle_fraud_tracking(sender, instance, created, **kwargs):
@@ -15,14 +17,25 @@ def handle_fraud_tracking(sender, instance, created, **kwargs):
         cache.set(f'{instance.ip_address}-whitelisted-ip', instance.pk)
         return
 
-    related_trackers = FraudTracker.objects.filter(ip_address=instance.ip_address)
+    expiration_delta = getattr(settings, 'DAYS_TO_EXPIRATION', None)
+    time_range = datetime.now() - timedelta(days=expiration_delta or 0)
+    import pdb; pdb.set_trace()
+    related_trackers = FraudTracker.objects.filter(ip_address=instance.ip_address, created_at__gt=time_range)
+
+    message = f'Site: {settings.SITE_NAME} \nIP address: {instance.ip_address}'
+    url_message = f'\nurl {instance.request_url}'
+    message += url_message
+    user = instance.user.username if instance.user else 'Anonymous User'
+    message += f'\nUser: {user}'
+    email =  instance.user_email if not instance.user else instance.user.email
+    message += f'\nEmail: {email}' if email else ''
+    alert_admin(f'A request has been determined fraudulent in your app, currently the strike count is: {related_trackers.count()}', message)
 
     if related_trackers.count() >= settings.ALLOWED_STRIKES:
         urls = ', '.join(related_trackers.values_list('request_url', flat=True)) 
-        alert_admin(
-            'An IP address has been blocked in your application',
-            f'Site: {settings.SITE_NAME} \nIP address: {instance.ip_address} \nurl history: {urls}'
-        )
+        url_history = f'\nurl history: {urls}'
+        message += url_history
+        alert_admin('An IP address has been blocked in your application', message)
 
         response = block_ip_address(instance.ip_address)
         if response.json().get('success') == False:
@@ -42,22 +55,22 @@ def remove_whitelist_cache(sender, instance, **kwargs):
 def alert_admin(subject, message, from_email=None, **kwargs):
     send_mail(subject, message, from_email, [settings.ADMIN_EMAIL], **kwargs)
 
-def block_ip_address(ip_address):
-    headers = {
-        'X-Auth-Email': settings.CL_AUTH_EMAIL,
-        'X-Auth-Key': settings.CL_AUTH_KEY,
-        'Content-Type': 'application/json',
-    }
+# def block_ip_address(ip_address):
+#     headers = {
+#         'X-Auth-Email': settings.CL_AUTH_EMAIL,
+#         'X-Auth-Key': settings.CL_AUTH_KEY,
+#         'Content-Type': 'application/json',
+#     }
 
-    data = {
-        'ip': ip_address,
-        'comment': f'blocked ip for {settings.SITE_NAME}' 
-    }
+#     data = {
+#         'ip': ip_address,
+#         'comment': f'blocked ip for {settings.SITE_NAME}' 
+#     }
 
-    response = requests.post(
-        f'https://api.cloudflare.com/client/v4/accounts/{settings.CL_ACCOUNT_ID}/rules/lists/{settings.CL_LIST_ID}/items',
-        headers=headers,
-        data=f'[{json.dumps(data)}]'
-    )
+#     response = requests.post(
+#         f'https://api.cloudflare.com/client/v4/accounts/{settings.CL_ACCOUNT_ID}/rules/lists/{settings.CL_LIST_ID}/items',
+#         headers=headers,
+#         data=f'[{json.dumps(data)}]'
+#     )
 
-    return response
+#     return response
