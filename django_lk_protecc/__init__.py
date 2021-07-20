dc24375e60647e3ea55a7aec6f74f6a9b4a8e246
Andrew Rasmussen <drewrasm@gmail.com>
2021-07-20 15:37:34 -0600

    fix uername bug, add util function, update version

--- django_lk_protecc/__init__.py
+++ django_lk_protecc/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'django_lk_protecc'
-__version__ = '1.0.1'
+__version__ = 'v1.1.0'
 __author__ = 'lightningkite'
 __license__ = 'tba'
 __copyright__ = 'Copyright 2021 lightningkite'
--- django_lk_protecc/protecc/utils.py
+++ django_lk_protecc/protecc/utils.py
@@ -19,7 +19,6 @@ def block_ip_address(ip_address):
         headers=headers,
         data=f'[{json.dumps(data)}]'
     )
-
     return response
 
 def remove_blocked_ip_address(ip_address):
@@ -39,5 +38,14 @@ def remove_blocked_ip_address(ip_address):
     data = {"items":[{"id": list_item_id}]}
 
     response = requests.delete(f'https://api.cloudflare.com/client/v4/accounts/{settings.CL_ACCOUNT_ID}/rules/lists/{settings.CL_LIST_ID}/items', headers=headers, data=json.dumps(data))
+    return response
 
+def get_cloudflare_list_items():
+    headers = {
+        'X-Auth-Email': settings.CL_AUTH_EMAIL,
+        'X-Auth-Key': settings.CL_AUTH_KEY,
+        'Content-Type': 'application/json',
+    }
+
+    response = requests.get(f'https://api.cloudflare.com/client/v4/accounts/{settings.CL_ACCOUNT_ID}/rules/lists/{settings.CL_LIST_ID}/items', headers=headers)
     return response
